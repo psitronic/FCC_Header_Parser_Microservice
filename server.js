fcd052e5cf81371611e323e2945bfc0ba5fd1c76
psitronic <spintronic@tuta.io>
2018-06-05 13:00:22 +0200

    Fix bug with IP address request

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 var express = require('express');
+var reqIP = require("request-ip");
 var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
 
 app.get("/api/whoami", (req, res, next) => {
   var ua = req.get('User-Agent');
   var al = req.get('Accept-Language');
   res.json({
-    "ipaddress": req.ip,
+    "ipaddress": reqIP.getClientIp(req),
     "language": al,
     "software": ua
   });
